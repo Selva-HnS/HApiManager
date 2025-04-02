ec7671493328681bdd8f8ac45221803d20ea5073
Thirumalai Ganesh G <thirumalaiganeshg@hnsonline.com>
2025-04-02 17:39:30 +0530

    img/file upload

--- Sources/HApiManager/HApiManager.swift
+++ Sources/HApiManager/HApiManager.swift
@@ -4,6 +4,8 @@
 import Foundation
 import Combine
 import SSLPinningManager
+import UIKit
+import UniformTypeIdentifiers
 
 public enum HTTPMethod: String {
     /// `CONNECT` method.
@@ -26,6 +28,20 @@ public enum HTTPMethod: String {
     case query = "QUERY"
     /// `TRACE` method.
     case trace = "TRACE"
+}
+
+struct Media {
+    let key: String
+    let filename: String
+    let data: Data
+    let mimeType: String
+    init?(withImage image: UIImage, forKey key: String) {
+        self.key = key
+        self.mimeType = "image/png"//"image/jpeg"
+        self.filename = "imagefile.png"//"imagefile.jpg"
+        guard let data = image.jpegData(compressionQuality: 0.7) else { return nil }
+        self.data = data
+    }
 }
 
 @MainActor class SSLPinningDelegate: NSObject, URLSessionDelegate {
@@ -57,6 +73,8 @@ public enum HTTPMethod: String {
         ssldomainkeys : [String: [String:[String]]]? = nil,
         timeIntervalRequest: Double = 30.0,
         timeIntervalResource: Double = 60.0,
+        image: UIImage? = nil,
+        fileurl: URL? = nil,
         completion: @escaping (Result<(Int, Data), Error>) -> Void)  {
             
             // Add Endpoint url
@@ -70,9 +88,21 @@ public enum HTTPMethod: String {
             request.httpMethod = method.rawValue
                         
             // Add Headers
+            var urlencoded : Bool = false
+            let boundary = "Boundary-\(UUID().uuidString)"
+
             if headers != nil {
                 headers?.forEach { key, value in
-                    request.addValue(value, forHTTPHeaderField: key)
+                    if (image != nil || fileurl != nil) {
+                        if !(value.contains("multipart/form-data; boundary=")) {
+                            request.addValue(value, forHTTPHeaderField: key)
+                        }
+                    } else {
+                        if value.contains("x-www-form-urlencoded") {
+                            urlencoded = true
+                        }
+                        request.addValue(value, forHTTPHeaderField: key)
+                    }
                 }
             } else {
                 request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -83,6 +113,43 @@ public enum HTTPMethod: String {
                 request.httpBody = soaptxt.data(using: .utf8)!
             }
 
+            // Add multipart/form-data
+            if (image != nil || fileurl != nil) {
+                request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+            }
+
+            // Add Image
+            if let img = image, image != nil {
+                let convertedImage = img.resized(toWidth: 1024.0) ?? UIImage()
+                let mediaImage = Media(withImage: convertedImage, forKey: "file")
+                
+                // Create multipart body
+                let dataBody = createDataBody(withParameters: nil, media: [mediaImage!], boundary: boundary)
+                request.httpBody = dataBody
+            }
+            
+            // Add File such as pdf, xlsx, doc
+            if let fUrl = fileurl, fileurl != nil {
+                let fileName = fUrl.lastPathComponent
+                let mimetype = mimeType(for: fileName)
+                let haveAccess = fUrl.startAccessingSecurityScopedResource()
+                defer {
+                    DispatchQueue.main.async {
+                        fUrl.stopAccessingSecurityScopedResource()
+                    }
+                }
+                var fileData: Data?
+                if haveAccess {
+                    fileData = try? Data(contentsOf: fUrl)
+                }
+                
+                // Create multipart body
+                let dataBody = createDataBody(fileData: fileData, boundary: boundary, mimetype: mimetype, fileName: fileName, fileURL: fUrl)
+                
+                request.setValue(String(dataBody.count), forHTTPHeaderField: "Content-Length")
+                request.httpBody = dataBody
+            }
+
             // Add Parameters (Query for GET, Body for others)
             if let parameters = parameters {
                 if method == .get {
@@ -90,7 +157,11 @@ public enum HTTPMethod: String {
                     components?.queryItems = parameters.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
                     request.url = components?.url
                 } else {
-                    request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
+                    if urlencoded {
+                        request.httpBody = parameters.percentEncoded()
+                    } else {
+                        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
+                    }
                 }
             }
             
@@ -149,11 +220,14 @@ public enum HTTPMethod: String {
     public func request<T: Codable>(
         endpoint: String,
         method: HTTPMethod = .get,
+        soapMessage: String? = nil,
         parameters: [String: Any]? = nil,
         headers: [String: String]? = nil,
         ssldomainkeys : [String: [String:[String]]]? = nil,
         timeIntervalRequest: Double = 30.0,
         timeIntervalResource: Double = 60.0,
+        image: UIImage? = nil,
+        fileurl: URL? = nil,
         completion: @escaping (Result<T, Error>) -> Void) {
             
             // Add Endpoint url
@@ -167,14 +241,68 @@ public enum HTTPMethod: String {
             request.httpMethod = method.rawValue
                         
             // Add Headers
+            var urlencoded : Bool = false
+            let boundary = "Boundary-\(UUID().uuidString)"
+
             if headers != nil {
                 headers?.forEach { key, value in
-                    request.addValue(value, forHTTPHeaderField: key)
+                    if (image != nil || fileurl != nil) {
+                        if !(value.contains("multipart/form-data; boundary=")) {
+                            request.addValue(value, forHTTPHeaderField: key)
+                        }
+                    } else {
+                        if value.contains("x-www-form-urlencoded") {
+                            urlencoded = true
+                        }
+                        request.addValue(value, forHTTPHeaderField: key)
+                    }
                 }
             } else {
                 request.addValue("application/json", forHTTPHeaderField: "Content-Type")
             }
             
+            // Add Soap Message
+            if let soaptxt = soapMessage, soaptxt != "" {
+                request.httpBody = soaptxt.data(using: .utf8)!
+            }
+
+            // Add multipart/form-data
+            if (image != nil || fileurl != nil) {
+                request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+            }
+
+            // Add Image
+            if let img = image, image != nil {
+                let convertedImage = img.resized(toWidth: 1024.0) ?? UIImage()
+                let mediaImage = Media(withImage: convertedImage, forKey: "file")
+                
+                // Create multipart body
+                let dataBody = createDataBody(withParameters: nil, media: [mediaImage!], boundary: boundary)
+                request.httpBody = dataBody
+            }
+            
+            // Add File such as pdf, xlsx, doc
+            if let fUrl = fileurl, fileurl != nil {
+                let fileName = fUrl.lastPathComponent
+                let mimetype = mimeType(for: fileName)
+                let haveAccess = fUrl.startAccessingSecurityScopedResource()
+                defer {
+                    DispatchQueue.main.async {
+                        fUrl.stopAccessingSecurityScopedResource()
+                    }
+                }
+                var fileData: Data?
+                if haveAccess {
+                    fileData = try? Data(contentsOf: fUrl)
+                }
+                
+                // Create multipart body
+                let dataBody = createDataBody(fileData: fileData, boundary: boundary, mimetype: mimetype, fileName: fileName, fileURL: fUrl)
+                
+                request.setValue(String(dataBody.count), forHTTPHeaderField: "Content-Length")
+                request.httpBody = dataBody
+            }
+
             // Add Parameters (Query for GET, Body for others)
             if let parameters = parameters {
                 if method == .get {
@@ -182,7 +310,11 @@ public enum HTTPMethod: String {
                     components?.queryItems = parameters.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
                     request.url = components?.url
                 } else {
-                    request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
+                    if urlencoded {
+                        request.httpBody = parameters.percentEncoded()
+                    } else {
+                        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
+                    }
                 }
             }
             
@@ -228,4 +360,90 @@ public enum HTTPMethod: String {
             // 6. saving the subscriber into an AnyCancellable Set (without this step this won't work)
                 .store(in: &cancellables)
         }
+    
+    private func createDataBody(withParameters params: [String: Any]?, media: [Media]?, boundary: String) -> Data {
+        
+        let lineBreak = "\r\n"
+        var body = Data()
+        
+        if let parameters = params {
+            for (key, value) in parameters {
+                let val = (value as? String) ?? "\(value)"
+                body.append("--\(boundary + lineBreak)".data(using: .utf8)!)
+                body.append("Content-Disposition: form-data; name=\"\(key)\"\(lineBreak + lineBreak)".data(using: .utf8)!)
+                body.append("\(val + lineBreak)".data(using: .utf8)!)
+            }
+        }
+        if let media = media {
+            for photo in media {
+                body.append("--\(boundary + lineBreak)".data(using: .utf8)!)
+                body.append("Content-Disposition: form-data; name=\"\(photo.key)\"; filename=\"\(photo.filename)\"\(lineBreak)".data(using: .utf8)!)
+                body.append("Content-Type: \(photo.mimeType + lineBreak + lineBreak)".data(using: .utf8)!)
+                body.append(photo.data)
+                body.append(lineBreak.data(using: .utf8)!)
+                print("mimeType \(photo.mimeType)")
+            }
+        }
+        body.append("--\(boundary)--\(lineBreak)".data(using: .utf8)!)
+        
+        return body as Data
+    }
+    
+    func generateBoundary() -> String {
+        return "Boundary-\(NSUUID().uuidString)"
+    }
+    
+    func mimeType(for path: String) -> String {
+        if let mimeType = UTType(filenameExtension: URL(fileURLWithPath: path).pathExtension)?.preferredMIMEType {
+            return mimeType
+        }
+        else {
+            return "application/octet-stream"
+        }
+    }
+
+    private func createDataBody(fileData: Data?, boundary: String, mimetype: String, fileName: String, fileURL: URL?) -> Data {
+        var body = Data()
+        
+        let lineBreak = "\r\n"
+        body.append("--\(boundary + lineBreak)".data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name=\"file\"; filename=\"\(fileName)\"\(lineBreak)".data(using: .utf8)!)
+        body.append("Content-Type: \(mimetype + lineBreak + lineBreak)".data(using: .utf8)!)
+        if let data = fileData {
+            body.append(data)
+        } else {
+            if let fileurl = fileURL {
+                body.append("\(fileurl)".data(using: .utf8)!)
+            }
+        }
+        body.append(lineBreak.data(using: .utf8)!)
+        body.append("--\(boundary)--\(lineBreak)".data(using: .utf8)!)
+
+
+        return body
+    }
+
+}
+
+extension Dictionary {
+    func percentEncoded() -> Data? {
+        map { key, value in
+            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+            return escapedKey + "=" + escapedValue
+        }
+        .joined(separator: "&")
+        .data(using: .utf8)
+    }
+}
+
+extension UIImage {
+    func resized(toWidth width: CGFloat, isOpaque: Bool = true) -> UIImage? {
+        let canvas = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+        let format = imageRendererFormat
+        format.opaque = isOpaque
+        return UIGraphicsImageRenderer(size: canvas, format: format).image {
+            _ in draw(in: CGRect(origin: .zero, size: canvas))
+        }
+    }
 }
