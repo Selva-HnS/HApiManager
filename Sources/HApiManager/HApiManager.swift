f3f210c8eda290aa5c17a2506691cd32a73a9fc4
Thirumalai Ganesh G <thirumalaiganeshg@hnsonline.com>
2025-04-03 10:28:43 +0530

    Error handling with status code

--- Sources/HApiManager/HApiManager.swift
+++ Sources/HApiManager/HApiManager.swift
@@ -30,6 +30,34 @@ public enum HTTPMethod: String {
     case trace = "TRACE"
 }
 
+public enum APIError: LocalizedError {
+    case invalidResponse(statusCode: Int)
+    case decodingError
+    case unknownError
+
+    public var errorDescription: String? {
+        switch self {
+        case .invalidResponse(let statusCode):
+            return "Request failed with status code: \(statusCode)"
+        case .decodingError:
+            return "Failed to decode the response."
+        case .unknownError:
+            return "An unknown error occurred."
+        }
+    }
+    
+    public var responseCode: Int? {
+        switch self {
+        case .invalidResponse(let statusCode):
+            return statusCode
+        case .decodingError:
+            return 0
+        case .unknownError:
+            return 520
+        }
+    }
+}
+
 struct Media {
     let key: String
     let filename: String
@@ -75,6 +103,7 @@ struct Media {
         timeIntervalResource: Double = 60.0,
         image: UIImage? = nil,
         fileurl: URL? = nil,
+        successStatusCodes: [Int] = [200,201,202,203,204,205,206,207,208,209],
         completion: @escaping (Result<(Int, Data), Error>) -> Void)  {
             
             // Add Endpoint url
@@ -146,6 +175,7 @@ struct Media {
                 // Create multipart body
                 let dataBody = createDataBody(fileData: fileData, boundary: boundary, mimetype: mimetype, fileName: fileName, fileURL: fUrl)
                 
+                // Add Content-Length
                 request.setValue(String(dataBody.count), forHTTPHeaderField: "Content-Length")
                 request.httpBody = dataBody
             }
@@ -190,11 +220,17 @@ struct Media {
             
             // 3. Use 'tryMap'(Operator) to get the data from the result
                 .tryMap({ (data, response) -> (Int, Data) in
-                    guard let response = response as? HTTPURLResponse,
-                          response.statusCode >= 200 else {
-                        throw URLError(.badServerResponse)
+                    guard let response = response as? HTTPURLResponse else {
+                        throw APIError.unknownError
                     }
-                    return (response.statusCode, data)
+
+                    // ✅ Default accept status codes in [200-209]
+                    if successStatusCodes.contains(response.statusCode) {
+                        return (response.statusCode, data)
+                    } else {
+                        // Handle error status codes
+                        throw APIError.invalidResponse(statusCode: response.statusCode)
+                    }
                 })
             
             //.map { $0.data }
@@ -210,152 +246,6 @@ struct Media {
                     }
                 } receiveValue: { (resultArr) in
                     completion(.success((resultArr.0, resultArr.1)))
-                }
-            // 6. saving the subscriber into an AnyCancellable Set (without this step this won't work)
-                .store(in: &cancellables)
-        }
-    
-    //FuturePublisher with return T
-    
-    public func request<T: Codable>(
-        endpoint: String,
-        method: HTTPMethod = .get,
-        soapMessage: String? = nil,
-        parameters: [String: Any]? = nil,
-        headers: [String: String]? = nil,
-        ssldomainkeys : [String: [String:[String]]]? = nil,
-        timeIntervalRequest: Double = 30.0,
-        timeIntervalResource: Double = 60.0,
-        image: UIImage? = nil,
-        fileurl: URL? = nil,
-        completion: @escaping (Result<T, Error>) -> Void) {
-            
-            // Add Endpoint url
-            guard let url = URL(string: endpoint) else {
-                completion(.failure(URLError(.badURL)))
-                return
-            }
-            
-            // Define request
-            var request = URLRequest(url: url)
-            request.httpMethod = method.rawValue
-                        
-            // Add Headers
-            var urlencoded : Bool = false
-            let boundary = "Boundary-\(UUID().uuidString)"
-
-            if headers != nil {
-                headers?.forEach { key, value in
-                    if (image != nil || fileurl != nil) {
-                        if !(value.contains("multipart/form-data; boundary=")) {
-                            request.addValue(value, forHTTPHeaderField: key)
-                        }
-                    } else {
-                        if value.contains("x-www-form-urlencoded") {
-                            urlencoded = true
-                        }
-                        request.addValue(value, forHTTPHeaderField: key)
-                    }
-                }
-            } else {
-                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            }
-            
-            // Add Soap Message
-            if let soaptxt = soapMessage, soaptxt != "" {
-                request.httpBody = soaptxt.data(using: .utf8)!
-            }
-
-            // Add multipart/form-data
-            if (image != nil || fileurl != nil) {
-                request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-            }
-
-            // Add Image
-            if let img = image, image != nil {
-                let convertedImage = img.resized(toWidth: 1024.0) ?? UIImage()
-                let mediaImage = Media(withImage: convertedImage, forKey: "file")
-                
-                // Create multipart body
-                let dataBody = createDataBody(withParameters: nil, media: [mediaImage!], boundary: boundary)
-                request.httpBody = dataBody
-            }
-            
-            // Add File such as pdf, xlsx, doc
-            if let fUrl = fileurl, fileurl != nil {
-                let fileName = fUrl.lastPathComponent
-                let mimetype = mimeType(for: fileName)
-                let haveAccess = fUrl.startAccessingSecurityScopedResource()
-                defer {
-                    DispatchQueue.main.async {
-                        fUrl.stopAccessingSecurityScopedResource()
-                    }
-                }
-                var fileData: Data?
-                if haveAccess {
-                    fileData = try? Data(contentsOf: fUrl)
-                }
-                
-                // Create multipart body
-                let dataBody = createDataBody(fileData: fileData, boundary: boundary, mimetype: mimetype, fileName: fileName, fileURL: fUrl)
-                
-                request.setValue(String(dataBody.count), forHTTPHeaderField: "Content-Length")
-                request.httpBody = dataBody
-            }
-
-            // Add Parameters (Query for GET, Body for others)
-            if let parameters = parameters {
-                if method == .get {
-                    var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
-                    components?.queryItems = parameters.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
-                    request.url = components?.url
-                } else {
-                    if urlencoded {
-                        request.httpBody = parameters.percentEncoded()
-                    } else {
-                        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
-                    }
-                }
-            }
-            
-            // 1. Create 'dataTaskPusblisher'(Publisher) to make the API call with ssl keys
-            let urlSession: URLSession = {
-                let serverConfig = ssldomainkeys ?? [:]
-                SSLPinningController.shared.setCofiGuration(configuration: serverConfig)
-                
-                let configuration = URLSessionConfiguration.default
-                configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
-                let cache = URLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil) // No cache
-                configuration.urlCache = cache
-                configuration.requestCachePolicy = .reloadIgnoringLocalCacheData // Disable cache
-                configuration.connectionProxyDictionary = nil
-                configuration.timeoutIntervalForRequest = timeIntervalRequest
-                configuration.timeoutIntervalForResource = timeIntervalResource
-
-                return URLSession(configuration: configuration, delegate: SSLPinningDelegate(), delegateQueue: OperationQueue())
-            }()
-            
-            urlSession.dataTaskPublisher(for: request)
-            //URLSession.shared.dataTaskPublisher(for: request)
-            
-            // 2. Make this process in main thread. (you can do this in background thread as well)
-                .receive(on: DispatchQueue.main)
-            
-            // 3. Use 'map'(Operator) to get the data from the result
-                .map { $0.data }
-            // 4. Decode the data into the 'Decodable' struct using JSONDecoder
-                .decode(type: T.self, decoder: JSONDecoder())
-            
-            // 5. Use 'sink'(Subcriber) to get the decoaded value or error, and pass it to completion handler
-                .sink { (resultCompletion) in
-                    switch resultCompletion {
-                    case .failure(let error):
-                        completion(.failure(error))
-                    case .finished:
-                        return
-                    }
-                } receiveValue: { (resultArr) in
-                    completion(.success((resultArr)))
                 }
             // 6. saving the subscriber into an AnyCancellable Set (without this step this won't work)
                 .store(in: &cancellables)
