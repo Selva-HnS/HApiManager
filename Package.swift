c34f859c282057b6adc438dd21b644cbce3e358f
arunpandian.c <arunpandianc@hnsonline.com>
2025-04-29 17:01:38 +0530

    SSLpinning package updated

--- Package.swift
+++ Package.swift
@@ -14,16 +14,20 @@ let package = Package(
             name: "HApiManager",
             targets: ["HApiManager"]),
     ],
+    dependencies: [
+        // Add your SPM dependency here
+        .package(url: "https://github.com/Selva-HnS/SSLPinningManager.git", from: "1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "HApiManager",
-            dependencies: ["SSLPinningManager"]),
-        .binaryTarget(
-                    name: "SSLPinningManager",
-                    path: "./Sources/SSLPinningManager.xcframework"
-                ),
+            dependencies: [.product(name: "SSLPinningManager", package: "SSLPinningManager")]),
+//        .binaryTarget(
+//            name: "SSLPinningManager",
+//            path: "./Sources/SSLPinningManager.xcframework"
+//        ),
         .testTarget(
             name: "HApiManagerTests",
             dependencies: ["HApiManager"]
